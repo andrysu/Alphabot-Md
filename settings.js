16868afc173e73dd5394580049e723b9ef225006
andrysu <97227953+andrysu@users.noreply.github.com>
2022-04-05 06:05:08 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,8 +31,8 @@ global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder i
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6285894148101","6285894148101","6285894148101"] //ganti agar fitur owner bisa di gunakan
-global.packname = 'Punya Andry' //sticker wm ubah
-global.author = 'Cieee mau nyolong' //sticker wm ganti nama kalian
+global.packname = 'Createdby' //sticker wm ubah
+global.author = '𝘼𝙣𝙙𝙧𝙮𝙬𝞪' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
